745954851693f9fd207869748c8dc611818cab03
Regeln <regeln17@protonmail.com>
2023-10-31 17:00:16 +0200

    Commit task 21

--- Basic_JS/practice_21/1.js
+++ Basic_JS/practice_21/1.js
@@ -16,15 +16,22 @@
 // так що лише перший символ у верхньому регістрі, інші у нижньому, 
 // об'єднує їх в одну стрічку з символом'-' -> виводить в консоль 
 // 'Result: Custom-Web-And-Mobile-Platforms length: 31'
-const arr = ["CusTom", "Web", "aNd", "MoBile", "PlaTfoRms"];
-     
-let compose = (...functions) => (data) => functions.reduceRight((value, func) => func(value), data);
 
-let modifyArray = (modifyCondition) => (data) => data.map(modifyCondition);
- 
-let capitalizeAllFirst; // use compose + modifyArray here
- 
-let allToLower; // use compose + modifyArray here
- 
-// capitalizeAllFirst(arr) // в консолі 'Result: Custom-Web-And-Mobile-Platforms length: 31'
-// allToLower(arr) // в консолі 'Result: custom - web - and - mobile - platforms length: 39'
+const compose = (...functions) => (data) => functions.reduceRight((value, func) => func(value), data);
+
+const modifyArray = (modifyCondition) => (data) => data.map(modifyCondition);
+
+const capitalizeAllFirst = compose(
+    (arr) => arr.join(' - '),
+    modifyArray((str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase())
+);
+
+const allToLower = compose(
+    (arr) => arr.join('-'),
+    modifyArray((str) => str.toLowerCase())
+);
+
+const testArray = ["CusTom", "Web", "aNd", "MoBile", "PlaTfoRms"];
+
+console.log('Result:', capitalizeAllFirst(testArray), 'length:', capitalizeAllFirst(testArray).length);
+console.log('Result:', allToLower(testArray), 'length:', allToLower(testArray).length);
